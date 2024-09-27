a4c934a8b1692e322f8b76d8db499f82e59c7289
Diego Vega Víquez <DIEGO.VEGAVIQUEZ@ucr.ac.cr>
2024-09-26 21:57:52 -0600

    feat: crea primer gráfico

--- munge/Graficos.R
+++ munge/Graficos.R
@@ -20,6 +20,7 @@ library(cherryblossom)
 library(usdata)
 library(openintro)
 library(haven) # Para cargar el .sav
+library(scales)
 
 
 datos_con_tipo_de_varibles %>%
@@ -28,4 +29,17 @@ datos_con_tipo_de_varibles %>%
   geom_bar(colour = "black") +
   labs( x = "Títulos", 
         y = "Cantidad")
+
+variables_utiles %>% ggplot(aes(x = NivInst,
+                                y = ipnh,
+                                fill = NivInst)) +
+                     geom_boxplot() +
+                     scale_y_log10(labels = label_number()) +
+                     labs( title = "Figura 1. Comparación del Ingreso Principal Neto del Hogar según Nivel de Instrucción, 2023",
+                           subtitle = "Expresado en colones costarricenses",
+                           x = "Nivel de Instrucción",
+                           y = "Ingreso Principal Neto del Hogar",
+                           fill =  "Nivel de Instrucción",
+                           caption = "Fuente: Instituto Nacional de Estadística y Censos (INEC), Costa Rica. (2023). Encuesta Nacional de Hogares 2023, Julio 2023: Resultados Generales.")+
+                     theme(axis.text.x = element_blank())
 
